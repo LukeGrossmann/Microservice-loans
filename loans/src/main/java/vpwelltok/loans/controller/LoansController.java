e6b3680ce5e5bd0504cb21d74abfd57a6d318449
Luke Grossmann <luke.grossmann@virginpulse.com>
2023-03-30 15:43:47 -0700

    Update loans microservice to connect to server microservice

--- loans/src/main/java/vpwelltok/loans/controller/LoansController.java
+++ loans/src/main/java/vpwelltok/loans/controller/LoansController.java
@@ -2,25 +2,29 @@ package vpwelltok.loans.controller;
 
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import vpwelltok.loans.config.LoansServiceConfig;
+import vpwelltok.loans.model.Properties;
 import vpwelltok.loans.model.Customer;
 import vpwelltok.loans.model.Loans;
 import vpwelltok.loans.repository.LoansRepository;
 
-/**
- * @author Eazy Bytes
- *
- */
 
 @RestController
 public class LoansController {
 
     @Autowired
     private LoansRepository loansRepository;
+
+    @Autowired
+    LoansServiceConfig loansConfig;
 
     @PostMapping("/myLoans")
     public List<Loans> getLoansDetails(@RequestBody Customer customer) {
@@ -31,6 +35,15 @@ public class LoansController {
             return null;
         }
 
+    }
+
+    @GetMapping("/loans/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(loansConfig.getMsg(), loansConfig.getBuildVersion(),
+                loansConfig.getMailDetails(), loansConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 
 }
